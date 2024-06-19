bf07add6a21832b6f5f7e6bb6d261ba1e001958b
ExShare074 <168080573+ExShare074@users.noreply.github.com>
2024-06-19 21:01:26 +0300

    add new code ISP

--- Barbara_Liskov.py
+++ Barbara_Liskov.py
@@ -1,13 +1,16 @@
 class Bird():
 
-    def __init__(self, name):
-        self.name = name
-
     def fly(self):
         print("птица летает")
 
-class Ping(Bird):
-    pass
+class Duck(Bird):
+    def fly(self):
+        print("Эта утка летает быстро")
 
-p = Ping("Сара")
-p.fly()
+def fly_in_the_sky(animal):
+    animal.fly()
+
+b = Bird()
+d = Duck()
+fly_in_the_sky(b)
+fly_in_the_sky(d)
